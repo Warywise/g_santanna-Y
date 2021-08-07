7b34ba43d936ac54385649cd8012ccba2f32ec4b
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-08-07 16:04:00 -0300

    Inicio projeto restauranSystem: -Todos os arquivos iniciais prontos, -Elementos da página já capturados no script, -CSS já estilizado basicamente, -HTML mostrando o rascunho do projeto inicial.

---
+++ restaurantProject/restaurantSystem.js
@@ -0,0 +1,15 @@
+const clientsSelector = document.getElementById('current-clients');
+const clientRemover = document.getElementById('remove-client');
+const clientAddInput = document.getElementById('add-client-input');
+const clientAddButton = document.getElementById('add-client-button');
+
+const foodAddInput = document.getElementById('add-food-input');
+const foodAddButton = document.getElementById('add-food-button');
+const drinkAddInput = document.getElementById('add-drink-input');
+const drinkAddButton = document.getElementById('add-drink-button');
+
+const optionsMenu = document.getElementById('menu-divs');
+const drinkOptions = document.getElementById('drink-options');
+const foodOptions = document.getElementById('food-options');
+
+const secondarySection = document.querySelector('.secondary-section');
